b2990a8a4b1db16a53ffc65b869f06ce6fdc88fc
nadermohasan <nadermohasan@gmail.com>
2026-04-16 23:07:01 +0300

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // ⬅️ تم تغيير BrowserRouter إلى HashRouter
 import Login from "./pages/Login";
 import AdminDashboard from './pages/AdminDashboard';
 import TeacherDashboard from './pages/TeacherDashboard';
@@ -8,7 +8,7 @@ import QuizResult from './pages/QuizResult';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>  {/* ⬅️ تم الاستبدال هنا */}
       <Routes>
         {/* صفحات الدخول */}
         <Route path="/" element={<Login />} />
@@ -23,15 +23,15 @@ export default function App() {
         <Route path="/quiz/:subjectId" element={<QuizPage />} />
         <Route path="/result" element={<QuizResult />} /> 
 
-        {/* صفحة تنبيه للمسارات غير الموجودة (اختياري) */}
+        {/* صفحة تنبيه للمسارات غير الموجودة */}
         <Route path="*" element={
           <div style={{ textAlign: 'center', marginTop: '100px', fontFamily: 'Cairo' }}>
             <h1>404</h1>
             <p>عذراً، هذه الصفحة غير موجودة.</p>
-            <a href="/">العودة للرئيسية</a>
+            <a href="#/">العودة للرئيسية</a>  {/* ⬅️ استخدم href="#/" بدلاً من "/" */}
           </div>
         } />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
